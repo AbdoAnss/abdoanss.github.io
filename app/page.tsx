384f559c177b59b007cda83f5c2558b9c9ccb70a
Abdessamad ANSSEM <141258252+AbdoAnss@users.noreply.github.com>
2025-11-13 19:54:57 +0100

    Update profile image URL in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -159,7 +159,7 @@ export default function Personal() {
             </p>
           </div>
           <img 
-            src="https://media.licdn.com/dms/image/v2/D4E03AQHoEl3C_rHuIg/profile-displayphoto-scale_400_400/B4EZjm08I8HgAk-/0/1756219295800?e=1762992000&v=beta&t=pjWjWol7ehCNj3MaMvDXPVGABGcHR3_nYFECaI_ZUjk" 
+            src="https://media.licdn.com/dms/image/v2/D4E03AQHoEl3C_rHuIg/profile-displayphoto-scale_400_400/B4EZjm08I8HgAk-/0/1756219295800?e=1764806400&v=beta&t=ImavqzE19xR1eppRplxRsjSnjvPGETukD0-FZa_ksxc" 
             alt="profile picture" 
             className="h-40 w-40 shrink-0 rounded-full object-cover ring-2 ring-zinc-400 shadow-sm dark:ring-zinc-800"
           />
